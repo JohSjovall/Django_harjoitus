55968d07548b49accaf0e6daa8efcf3e1d4ab967
JohanS <joalsj@utu.fi>
2019-01-15 20:32:24 +0200

    Valitallennus

--- Joohan/blog/views.py
+++ Joohan/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post contet',
+        'date_post': 'August 27, 2018',
+    },
+    {
+        'author': 'Jonne',
+        'title': 'Blog Post 2',
+        'content': 'jotain post contet',
+        'date_post': 'August 28, 2018',
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1> Moikka </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> Totanoin </h1>')
+    return render(request, 'blog/about.html')
