24dd8f4dc59657bdeebbb535994eae1dda31ae33
JohanS <joalsj@utu.fi>
2019-01-17 16:38:09 +0200

    database

--- Joohan/blog/views.py
+++ Joohan/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post contet',
-        'date_post': 'August 27, 2018',
-    },
-    {
-        'author': 'Jonne',
-        'title': 'Blog Post 2',
-        'content': 'jotain post contet',
-        'date_post': 'August 28, 2018',
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
